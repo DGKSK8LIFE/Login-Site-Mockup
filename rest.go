2210903e6dbfb445b8a8fac0a5e69d5a8bd67534
Tarek Ali <tarekali15@outlook.com>
2019-11-05 20:06:06 -0600

    working on it...

--- rest.go
+++ rest.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var html *template.Template
@@ -13,8 +17,17 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		username = r.FormValue("username")
+		db, _ := sql.Open("sqlite3", "./stored_logins.sqlite")
+		username := r.FormValue("username")
 		password := r.FormValue("password")
+		defer db.Close()
+		if len(username) > 0 && len(password) > 0 {
+			rows, _ := db.Query("SELECT * FROM LOGINS;")
+			for rows.Next() {
+				rows.Scan(&username, &password)
+				fmt.Printf("%s: %s\n", username, password)
+			}
+		}
 		html.ExecuteTemplate(w, "main.html", nil)
 	})
 	http.ListenAndServe(":8000", nil)
