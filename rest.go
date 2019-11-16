ea422b4ecce0ba45a1c5577fd30cd04136ba7770
Tarek Ali <tarekali15@outlook.com>
2019-11-16 17:56:17 -0600

    works the first time, bugs out when I try to go back to the home page

--- rest.go
+++ rest.go
@@ -58,27 +58,16 @@ func showCreateSite(w http.ResponseWriter, r *http.Request) {
 	}
 	usernameClientSide := r.FormValue("username")
 	passwordClientSide := r.FormValue("password")
+
 	defer db.Close()
 	htmlTwo.Execute(w, "create.html")
+
 	if len(usernameClientSide) > 0 && len(passwordClientSide) > 0 {
-		rows, _ := db.Query("SELECT * FROM LOGINS;")
-		var usernameOne string
-		var passwordOne string
-		for rows.Next() {
-			err := rows.Scan(&usernameOne, &passwordOne)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			if usernameClientSide == usernameOne {
-				fmt.Fprintln(w, "<h1 style='text-align: center;'>Account already exists! Choose a different username!</h1>")
-				break
-			} else {
-				var query = fmt.Sprintf("INSERT INTO LOGINS (username, password) VALUES (%s, %s);", usernameOne, passwordOne)
-				db.Query(query)
-				break
-			}
-
+		insertQ := fmt.Sprintf("INSERT INTO LOGINS (username, password)\nVALUES ('%s', '%s');", usernameClientSide, passwordClientSide)
+		prepQ, err := db.Prepare(insertQ)
+		if err != nil {
+			log.Fatal(err)
 		}
+		prepQ.Exec()
 	}
 }
