46e52102343db7cb896b13f40546ea72ff8de3b6
Tarek Ali <tarekali15@outlook.com>
2019-11-16 18:24:03 -0600

    changed the first function from anonymous, to declared/named

--- rest.go
+++ rest.go
@@ -21,34 +21,35 @@ func init() {
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		db, err := sql.Open("sqlite3", "./stored_logins.sqlite")
-		if err != nil {
-			log.Fatal(err)
-		}
-		usernameUserSide := r.FormValue("username")
-		passwordUserSide := r.FormValue("password")
-		defer db.Close()
-		html.ExecuteTemplate(w, "main.html", nil)
-		if len(usernameUserSide) > 0 && len(passwordUserSide) > 0 {
-			rows, _ := db.Query("SELECT * FROM LOGINS;")
-			var username string
-			var password string
-			for rows.Next() {
-				err := rows.Scan(&username, &password)
-				if err != nil {
-					panic(err)
-				}
-				if usernameUserSide == username && passwordUserSide == password {
-					fmt.Fprintln(w, "<h1 style='text-align: center;'>welcome back, cyka blyat!</h1>")
-					break
-				}
-			}
-		}
-	})
-
+	http.HandleFunc("/", showLoginPage)
 	http.HandleFunc("/create.html/", showCreateSite)
 	http.ListenAndServe(":8000", nil)
+}
+
+func showLoginPage(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("sqlite3", "./stored_logins.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	usernameUserSide := r.FormValue("username")
+	passwordUserSide := r.FormValue("password")
+	defer db.Close()
+	html.ExecuteTemplate(w, "main.html", nil)
+	if len(usernameUserSide) > 0 && len(passwordUserSide) > 0 {
+		rows, _ := db.Query("SELECT * FROM LOGINS;")
+		var username string
+		var password string
+		for rows.Next() {
+			err := rows.Scan(&username, &password)
+			if err != nil {
+				panic(err)
+			}
+			if usernameUserSide == username && passwordUserSide == password {
+				fmt.Fprintln(w, "<h1 style='text-align: center;'>welcome back, cyka blyat!</h1>")
+				break
+			}
+		}
+	}
 }
 
 func showCreateSite(w http.ResponseWriter, r *http.Request) {
@@ -56,10 +57,10 @@ func showCreateSite(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+
 	usernameClientSide := r.FormValue("username")
 	passwordClientSide := r.FormValue("password")
-
+	defer db.Close()
 	htmlTwo.Execute(w, "create.html")
 
 	if len(usernameClientSide) > 0 && len(passwordClientSide) > 0 {
@@ -69,6 +70,7 @@ func showCreateSite(w http.ResponseWriter, r *http.Request) {
 			log.Fatal(err)
 		}
 		prepQ.Exec()
+
 		fmt.Fprintf(w, "<h1 style='text-align: center;'>You made an account!</h1>")
 	}
 }
