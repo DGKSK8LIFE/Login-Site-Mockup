0ab2db9a2ef471cf7ceae6c799c30bcd8d5191b6
Tarek Ali <tarekali15@outlook.com>
2019-12-22 10:12:43 -0600

    fully working system

--- rest.go
+++ rest.go
@@ -11,68 +11,64 @@ import (
 )
 
 var (
-	html    *template.Template
-	htmlTwo *template.Template
+	loginSite  *template.Template
+	createSite *template.Template
 )
 
 func init() {
-	html = template.Must(template.ParseGlob("main.html"))
-	htmlTwo = template.Must(template.ParseFiles("create.html"))
+	loginSite = template.Must(template.ParseGlob("login.html"))
+	createSite = template.Must(template.ParseGlob("create.html"))
 }
 
 func main() {
-	http.HandleFunc("/", showLoginPage)
-	http.HandleFunc("/create.html/", showCreateSite)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		loginSite.ExecuteTemplate(w, "main.html", nil)
+	})
+	http.HandleFunc("/create.html", func(w http.ResponseWriter, r *http.Request) {
+		createSite.ExecuteTemplate(w, "create.html", nil)
+	})
+	http.HandleFunc("/login", userAuth)
+	http.HandleFunc("/create", createAccount)
 	http.ListenAndServe(":8000", nil)
 }
 
-func showLoginPage(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open("sqlite3", "./stored_logins.sqlite")
+func userAuth(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	db, err := sql.Open("sqlite3", "stored_logins.sqlite")
 	if err != nil {
 		log.Fatal(err)
 	}
-	usernameUserSide := r.FormValue("username")
-	passwordUserSide := r.FormValue("password")
 	defer db.Close()
-	defer db.Ping()
-	html.ExecuteTemplate(w, "main.html", nil)
-	if len(usernameUserSide) > 0 && len(passwordUserSide) > 0 {
-		rows, _ := db.Query("SELECT * FROM LOGINS;")
-		var username string
-		var password string
-		for rows.Next() {
-			err := rows.Scan(&username, &password)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if usernameUserSide == username && passwordUserSide == password {
-				fmt.Fprintln(w, "<h1 style='text-align: center;'>welcome back, cyka blyat!</h1>")
-				break
-			}
-		}
+	if row := rowExists(username, password, db); row == true {
+		fmt.Fprint(w, "<h1 style='text-align: center;'>Welcome!</h1>")
+	} else if row == false {
+		// deny access
+		loginSite.ExecuteTemplate(w, "login.html", nil)
 	}
 }
 
-func showCreateSite(w http.ResponseWriter, r *http.Request) {
-	db, err := sql.Open("sqlite3", "./stored_logins.sqlite")
+func createAccount(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	db, err := sql.Open("sqlite3", "stored_logins.sqlite")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	usernameClientSide := r.FormValue("username")
-	passwordClientSide := r.FormValue("password")
 	defer db.Close()
-	defer db.Ping()
-	htmlTwo.Execute(w, "create.html")
+	query := fmt.Sprintf("INSERT INTO logins (username, password) VALUES ('%s', '%s');", username, password)
+	db.Exec(query)
+}
 
-	if len(usernameClientSide) > 0 && len(passwordClientSide) > 0 {
-		insertQ := fmt.Sprintf("INSERT INTO LOGINS (username, password)\nVALUES ('%s', '%s');", usernameClientSide, passwordClientSide)
-		prepQ, err := db.Prepare(insertQ)
-		if err != nil {
-			log.Fatal(err)
-		}
-		prepQ.Exec()
-
-		fmt.Fprintf(w, "<h1 style='text-align: center;'>You made an account!</h1>")
+func rowExists(username, password string, db *sql.DB) bool {
+	var exists bool
+	query := fmt.Sprintf("SELECT * FROM ACCOUNTS WHERE username='%s' AND password='%s'", username, password)
+	if err := db.QueryRow(query).Scan(&username, &password); err != nil && err != sql.ErrNoRows {
+		log.Fatal("database error, we're fucked")
+	} else if err == sql.ErrNoRows {
+		exists = false
+	} else {
+		exists = true
 	}
+	return exists
 }
